ca7bf5c530aae2473b5e2445f967a9e03117e239
roraks24 <rsaini2407@gmail.com>
2026-07-16 21:41:03 +0530

    Day 4 - exception handeling for Api failures

--- py.py
+++ py.py
@@ -1,59 +1,80 @@
-
 import os
 from dotenv import load_dotenv
-from groq import Groq
+from groq import Groq, APIConnectionError, RateLimitError,APIStatusError
+import json
+
+if os.path.exists("conversation_history.json"):
+    try:
+        with open("conversation_history.json", "r") as f:
+            conversation_history = json.load(f)
+    except json.JSONDecodeError:
+        conversation_history = []
+else:
+    conversation_history = []
 
 load_dotenv()
 
 client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
- )
-        
+)
+
 def chat(temperature=1):
- 
- conversation_history = []
 
- while True:
+    while True:
 
-    user_input = input("Enter (or press # to end): ")
+        user_input = input("Enter (or press # to end): ")
 
-    if user_input == "#":
-       break
-       
-    conversation_history.append({"role": "user", "content": user_input})
+        if user_input == "#":
+            with open("conversation_history.json", "w") as f:
+                json.dump(conversation_history, f)
+            break
 
-    chat_completion = client.chat.completions.create(
-      
-     messages = conversation_history,
-       
-        model="llama-3.3-70b-versatile",
-        temperature=temperature
-    )
-    
+        conversation_history.append({"role": "user", "content": user_input})
+        
+        try:
+         chat_completion = client.chat.completions.create(
+            messages=conversation_history,
+            model="llama-3.3-70b-versatile",
+            temperature=temperature,
+        )
+        
+        except RateLimitError:
+         print("Rate limit hit! You are sending requests too fast. Wait and try again later.")
+         conversation_history.pop()
+         continue
 
-    print(chat_completion.choices[0].message.content)
-    conversation_history.append({"role" : "assistant", "content" : chat_completion.choices[0].message.content})
-    print("#-----------------------------------------#")
-    print(chat_completion.usage)
-    print("#-----------------------------------------#")
-    
+        except APIConnectionError:
+            print("Netwrok connection lost! Check your connection and try again.")
+            continue
 
-while True: 
+        except APIStatusError as e:
+            print(f"API returned an error. Status ({e.status_code}) : {e.message}")
+            conversation_history.pop()
+            continue
+
+        print(chat_completion.choices[0].message.content)
+        conversation_history.append({"role": "assistant", "content": chat_completion.choices[0].message.content})
+        print("#-----------------------------------------#")
+        print(chat_completion.usage)
+        print("#-----------------------------------------#")
+
+
+while True:
 
     print("1. Enter the prompt"
-        "\n2. Quit"
-        )
-    
+          "\n2. Quit"
+          )
+
     try:
-      choice = int(input("Enter choice: "))
+        choice = int(input("Enter choice: "))
 
     except ValueError:
-       print("invalid input!")
-       continue
-    
+        print("invalid input!")
+        continue
+
     if choice == 1:
         chat()
-        
+
     elif choice == 2:
         break
 
